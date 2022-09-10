417fe7dd128ec7feffec8a65ef449ab901f0eb73
feraska <45358056+feraska@users.noreply.github.com>
2022-09-10 21:46:06 +0300

    mongodb java

---
+++ src/main/java/mainpkg/Main.java
@@ -0,0 +1,26 @@
+package mainpkg;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.*;
+import org.bson.Document;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            MongoClient mongoClient = MongoClients.create("mongodb+srv://feras:123@react-db.5elia.mongodb.net/?retryWrites=true&w=majority");
+            MongoDatabase db = mongoClient.getDatabase("shopping-cart");
+            MongoCollection<Document> document = db.getCollection("users");
+            System.out.println("successful connection");
+            BasicDBObject basicBSONObject = new BasicDBObject();
+            basicBSONObject.put("email","feras.94.kasabri@gmail.com");
+            MongoCursor<Document> document1= document.find(basicBSONObject).iterator();
+            while (document1.hasNext()){
+                System.out.println(document1.next().getString("firstName"));
+            }
+        }
+        catch (Exception e){
+
+        }
+    }
+
+}
